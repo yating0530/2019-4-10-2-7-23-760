0fbb2998b45285997e4352b21ea150c622f808fb
cyt <1064786402@qq.com>
2019-05-31 21:47:28 +0800

    change main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price = 0;
+    if (inputs['distance']>=8) {
+    	price = 6 + 0.8*(8-2) + (0.8+0.8*50%)*(inputs['distance']-8);
+    }else if (inputs['distance']>=2) {
+    	price = 6 + 0.8*(inputs['distance']-2);
+    }else {
+    	price = 6;
+    }
+    price += 0.25*inputs['parkTime'];
+    return Math.round(price);
 };
