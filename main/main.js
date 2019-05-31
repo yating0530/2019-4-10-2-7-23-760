e7a4eea329cd6bd0a8346fe4ad34a54a2eba06a4
cyt <1064786402@qq.com>
2019-05-31 21:57:55 +0800

    change main.js again

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ module.exports = function main(inputs) {
     // write your code here...
     var price = 0;
     if (inputs['distance']>=8) {
-    	price = 6 + 0.8*(8-2) + (0.8+0.8*50%)*(inputs['distance']-8);
+    	price = 6 + 0.8*(8-2) + (0.8+0.8*0.5)*(inputs['distance']-8);
     }else if (inputs['distance']>=2) {
     	price = 6 + 0.8*(inputs['distance']-2);
     }else {
